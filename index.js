772ac724ba79ee9aef8350b71e06f5323ab0b4b8
tymiurge <tmrlabas@gmail.com>
2018-03-26 14:17:48 +0300

    making router works

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const bodyParser = require('body-parser')
 const app = express()
 const morgan = require('morgan')
 
+/*
+const Cat = mongoose.model('Cat', { name: String })
+app.get('/saver', (req, res) => {
+    //new Thread({title: req.body.title, author: req.body.author}).save();
+    console.log('!!! looking for a cat !!!')
+    const k = new Cat({name: 'Zildjian'})
+    k.save().then(() => console.log('meow'))
+    //new CardModel({type: 'simple', front: 'crank', back: 'crank1'}).save()
+    //.then(() => res.send('ok'))
+    //.catch(e => res.send(e))
+})
+*/
+
 
 /** APP CONFIGURATION */
 
@@ -11,60 +24,47 @@ app.use(morgan('dev'))
 
 /** configure body parser */
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 const PORT = 3000;
 
 /** DATABASE SETUP */
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0:27017/db1')
     .then(() => console.info('mongo DB connection established'))
     .catch(e => console.error(e));
 
-/*
-const CardBox = require('./app/card-box')
 
+// source: https://github.com/scotch-io/node-api/blob/master/server.js
 
-// Schemas
+/** ROUTERS */
+const router = express.Router()
 
-const CardSchema = new mongoose.Schema({
-    type: String,
-    front: String,
-    back: String
-})
-
-const CardModel = mongoose.model('Card', CardSchema)
-
-// REQUIRE MIDDLEWARE
-
-*/
-
-var options = { //specify options
-    host: `localhost:${PORT}`
-}
-
-//USE AS MIDDLEWARE
-app.use(bodyParser.json()); // add body parser
-
-app.get('/entry', (req, res) => {
-    res.send('this is an entry 111')
-    console.log('!!! entry !!!')
-})
-
-const Cat = mongoose.model('Cat', { name: String })
-app.get('/saver', (req, res) => {
-    //new Thread({title: req.body.title, author: req.body.author}).save();
-    console.log('!!! looking for a cat !!!')
-    const k = new Cat({name: 'Zildjian'})
-    k.save().then(() => console.log('meow'))
-        //new CardModel({type: 'simple', front: 'crank', back: 'crank1'}).save()
-        //.then(() => res.send('ok'))
-        //.catch(e => res.send(e))
+router.use((req, res, next) => {
+    console.log(`path: ${req.path}, from:${req.headers.host}, at:${req._startTime}`)
+    next();
 })
 
 
+router.get(
+    '/',
+    (req, res) => {
+        res.send('ok')
+        //res.json({message: 'smarati api server status: OK'})
+    }
+)
 
+router.route('/cards')
+    .get((req, res) => {
+        res.json({status: 'ok', data: [0, 1, 2], requestType: 'get'})
+    })
+    .post((req, res) => {
+        res.json({status: 'ok', data: [{title: 't1'}, {title: 't2'}], requestType: 'post'})
+    })
 
-app.listen(PORT, ()=>{
-    console.log('started');
+app.use('/api', router)
+
+/** START THE SERVER */
+app.listen(PORT, () => {
+    console.log(`server started at port ${PORT}`);
 });
