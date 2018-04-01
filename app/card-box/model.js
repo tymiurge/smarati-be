4525849dc4ed306ef7573fbeb69de83f778b2709
tymiurge <tmrlabas@gmail.com>
2018-04-01 21:59:30 +0300

    card model - box and simple types transformenrs implementation

--- app/card-box/model.js
+++ app/card-box/model.js
@@ -12,16 +12,55 @@ const Schema = new mongoose.Schema({
 
 const Model = mongoose.model('CardBox', Schema)
 
+/**
+ * this is transforment of data from request (which should be passed
+ * in flat format) into mongo object (wchich might be in complex hierarchical
+ * format). the idea is to keep clients login as simple as possible while
+ * the quering and saving logic is concentrated in model only
+ * 
+ * @common_data:
+ *      type: enum (box|simple), required
+ *      parent: ObjectId, if null (meaning the card is placed in root) 
+ *              - null will be spcified
+ *      tags: array, if null - empty array will be saved
+ * 
+ * any additional data fields are specified in each of transformers below.
+ */
 const cardFactory = {
+    /**
+     * @param data shap of:
+     *      - @common_data
+     *      - frontContent: string, is required
+     * @returns mongo model
+     */
     box: data => (new Model({
         type: data.type,
         sides: {
             front: {
-                content: data.sides.front.content
+                content: data.frontContent
             }
         },
         parent: data.parent,
-        tags: data.tags,
+        tags: data.tags || [],
+        createdAt: (new Date().getTime())
+    })),
+    /**
+     * @param data shape of:
+     *      - @common_data
+     *      -  
+     */
+    simple: data => (new Model({
+        type: data.type,
+        sides: {
+            front: {
+                content: data.frontContent
+            },
+            back: {
+                content: data.backContent
+            }
+        },
+        parent: data.parent,
+        tags: data.tags || [],
         createdAt: (new Date().getTime())
     }))
 }
