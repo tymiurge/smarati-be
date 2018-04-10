601ed80147839f6eaf4a923c582430864bf95056
tymiurge <tmrlabas@gmail.com>
2018-04-10 23:29:53 +0300

    DESIGN: learning pool API

---
+++ app/pool/api.js
@@ -0,0 +1,34 @@
+module.exports = router => {
+    /**
+     * add pool
+     */
+    router.route('/pool/add/').post((req, res) => {
+        
+    })
+
+    /**
+     * get pool content
+     * 
+     */
+    router.route('/pool/get/:id').get((req, res) => {
+        
+    }) 
+
+    /**
+     * 
+     */
+    router.route('/pool/update').post((res, req) => {
+
+    })
+
+    /**
+     * 
+     */
+    router.route('/pool/delete').post((res, req) => {
+
+    })
+    
+
+    return router
+    
+}
